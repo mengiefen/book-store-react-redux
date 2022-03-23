170fec25f74fa2bfd0ebbb1a82045309a5ba817a
mengiefen <mengiefen@gmail.com>
2022-03-23 10:37:00 +0300

    add Form component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Book from './components/Book/Book';
+import Form from './components/Form/Form';
 import Header from './components/Header/Header';
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <div className="App">
       <Header />
       <Book />
+      <Form />
     </div>
   );
 }
