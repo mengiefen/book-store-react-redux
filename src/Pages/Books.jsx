6e3f81bb71182eefeda9c19c7a4163f8cc3fdee4
mengiefen <mengiefen@gmail.com>
2022-03-23 13:01:55 +0300

    add Books react component

---
+++ src/Pages/Books.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Book from '../components/Book/Book';
+import Form from '../components/Form/Form';
+
+const Books = () => (
+  <div>
+    <Book />
+    <Form />
+  </div>
+);
+
+export default Books;
