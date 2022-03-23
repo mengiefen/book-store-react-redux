e1c5e7224000a8dbd2bf250b348481e6eb07e297
mengiefen <mengiefen@gmail.com>
2022-03-23 12:59:48 +0300

    add react router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import './App.css';
-import Book from './components/Book/Book';
-import Form from './components/Form/Form';
+import { Routes, Route } from 'react-router-dom';
 import Header from './components/Header/Header';
+import Books from './Pages/Books';
+import Categories from './Pages/Categories';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Book />
-      <Form />
+      <Routes>
+        <Route path="/" element={<Books />} />
+        <Route path="/categories" element={<Categories />} />
+      </Routes>
     </div>
   );
 }
