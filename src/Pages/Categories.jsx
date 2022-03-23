4f50836a5abf1b2857a5a728cf2fc5c2e13931f1
mengiefen <mengiefen@gmail.com>
2022-03-23 13:02:08 +0300

    add Categories react component

---
+++ src/Pages/Categories.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Categories() {
+  return (
+    <div style={{ color: 'red', marginTop: '10rem', fontSize: '2rem' }}>
+      Categories
+    </div>
+  );
+}
+
+export default Categories;
