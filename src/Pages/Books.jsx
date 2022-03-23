3a62ceb82a691752d2161320f68aed25bad5adc3
mengiefen <mengiefen@gmail.com>
2022-03-23 16:01:14 +0300

    pass book data to book element

--- src/Pages/Books.jsx
+++ src/Pages/Books.jsx
@@ -1,19 +1,27 @@
 import React, { useState } from 'react';
+import styled from 'styled-components';
 import Book from '../components/Book/Book';
 import Form from '../components/Form/Form';
 
+const Wrapper = styled.div`
+  padding: 0;
+  margin-top: 6.5rem;
+`;
+
 const Books = () => {
-  const [book, setBook] = useState([
+  const [books, setBooks] = useState([
     {
       category: 'Action',
       title: 'The Hunger Games',
       author: 'Suzanne Collins',
       progress: 66,
-      id: 1,
+      id: Date.now(),
     },
   ]);
-  const onAddBook = () => {
-    setBook([
+  const onAddBook = (e) => {
+    e.preventDefault();
+    setBooks([
+      ...books,
       {
         category: 'Action',
         title: 'The Hunger Games',
@@ -23,11 +31,16 @@ const Books = () => {
       },
     ]);
   };
+
   return (
-    <div>
-      <Book book={book} />
+    <Wrapper>
+      {books.map((book) => (
+        <div key={book.id} className="book-item">
+          <Book book={book} />
+        </div>
+      ))}
       <Form onAddBook={onAddBook} />
-    </div>
+    </Wrapper>
   );
 };
 
