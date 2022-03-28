759ee2f514917dc023f648f077e7a1ae5a0a1064
mengiefen <mengiefen@gmail.com>
2022-03-28 19:00:24 +0300

    add bookReducer function

---
+++ src/redux/books/books.js
@@ -0,0 +1,50 @@
+const ADD = 'bookStore/books/ADD';
+const REMOVE = 'bookStore/books/REMOVE';
+const initialState = [
+  {
+    category: 'Action',
+    title: 'The Hunger Games',
+    author: 'Suzanne Collins',
+    progress: 64,
+    id: 1,
+  },
+  {
+    category: 'Action',
+    title: 'Dune',
+    author: 'Frank Herbert',
+    progress: 88,
+    id: 2,
+  },
+  {
+    category: 'Economy',
+    title: 'Capital in the Twenty Century',
+    author: 'Suzanne Collins',
+    progress: 66,
+    id: 3,
+  },
+];
+
+export default function bookReducer(state = initialState, action) {
+  switch (action.type) {
+    case ADD:
+      return [...state, action.book];
+    case REMOVE:
+      return [...state].filter((book) => book.id !== action.id);
+    default:
+      return state;
+  }
+}
+
+export function addBook(book) {
+  return {
+    type: ADD,
+    book,
+  };
+}
+
+export function removeBook(id) {
+  return {
+    type: REMOVE,
+    id,
+  };
+}
