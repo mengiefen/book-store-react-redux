a5ebb358629b01efafcc2efb722b9375ff44f72f
mengiefen <mengiefen@gmail.com>
2022-03-28 21:22:44 +0300

    add removeBook reducer

--- src/components/Book/Book.jsx
+++ src/components/Book/Book.jsx
@@ -1,124 +1,51 @@
 import React from 'react';
-import styled from 'styled-components';
 import PropTypes from 'prop-types';
-import { CircularProgressbar } from 'react-circular-progressbar';
-import 'react-circular-progressbar/dist/styles.css';
-
-const Wrapper = styled.div`
-  width: 80%;
-  height: auto;
-  padding: 1rem 2rem;
-  border: 1px solid #e8e8e8;
-  border-radius: 4px;
-  margin: 0.938rem auto 0 auto;
-  box-shadow: 1px 1px 5px #e8e8e8, -1px -1px 5px #e8e8e8;
-  display: flex;
-  gap: 5%;
-  background-color: #fff;
-  transition: border 0.5s;
-
-  &:hover {
-    border: 1px solid #4386bf;
-  }
-`;
-
-const LeftWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: start;
-  flex: 2;
-`;
-
-const BookType = styled.h4`
-  color: #555;
-  padding-bottom: 0.3rem;
-`;
-
-const BookTitle = styled.h2`
-  color: #111;
-  font-size: 1.4rem;
-  margin-bottom: 0.2rem;
-`;
-
-const BookAuthor = styled.p`
-  font-size: 0.875rem;
-  margin-bottom: 1.5rem;
-  color: #4386bf;
-`;
-
-const ButtonWrapper = styled.div`
-  display: flex;
-  gap: 1rem;
-`;
-
-const OutlineButton = styled.button`
-  color: #4386bf;
-  font-size: 0.9rem;
-  outline: none;
-  border: none;
-  background-color: transparent;
-  cursor: pointer;
-`;
-
-const MiddleWrapper = styled.div`
-  display: flex;
-  align-items: center;
-  gap: 1.5rem;
-  flex: 1;
-`;
-
-const Percentage = styled.div`
-  color: #222;
-  font-size: 2rem;
-  font-family: 'Montserrat', sans-serif;
-`;
-
-const RightWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: start;
-`;
-
-const Chapter = styled.p`
-  color: #222;
-  font-size: 1rem;
-  padding-bottom: 1rem;
-`;
-
-const ButtonFill = styled.button`
-  background-color: #0290ff;
-  border: none;
-  outline: none;
-  border-radius: 4px;
-  color: #fff;
-  padding: 0.801rem 1.188rem 0.886rem 1.375rem;
-  box-shadow: 0 0 60px 0 rgba(0, 0, 0, 0.05);
-  font-weight: 600;
-  cursor: pointer;
-`;
+import { CircularProgressbar, buildStyles } from 'react-circular-progressbar';
+import { useDispatch } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import * as actionCreators from '../../redux/books/books';
+import { ButtonFill, ButtonOutline } from '../Button/Button.styled';
+import {
+  Wrapper,
+  LeftWrapper,
+  MiddleWrapper,
+  RightWrapper,
+  ButtonWrapper,
+} from './Book.style';
 
 const Book = (props) => {
   const { book } = props;
+  const dispatch = useDispatch();
+  const { removeBook } = bindActionCreators(actionCreators, dispatch);
 
   return (
     <Wrapper key={book.id}>
       <LeftWrapper>
-        <BookType>{book.category}</BookType>
-        <BookTitle>{book.title}</BookTitle>
-        <BookAuthor>{book.author}</BookAuthor>
+        <h4>{book.category}</h4>
+        <h2>{book.title}</h2>
+        <p>{book.author}</p>
         <ButtonWrapper>
-          <OutlineButton>Comment</OutlineButton>
-          <OutlineButton>Remove</OutlineButton>
-          <OutlineButton>Edit</OutlineButton>
+          <ButtonOutline>Comment</ButtonOutline>
+          <ButtonOutline onClick={() => removeBook(book.id)}>
+            Remove
+          </ButtonOutline>
+          <ButtonOutline>Edit</ButtonOutline>
         </ButtonWrapper>
       </LeftWrapper>
 
       <MiddleWrapper>
         <div style={{ width: 60, height: 60 }}>
-          <CircularProgressbar value={book.progress} maxValue={100} />
+          <CircularProgressbar
+            value={book.progress}
+            maxValue={100}
+            styles={buildStyles({
+              textSize: '16px',
+              pathColor: '#0290ff',
+            })}
+          />
         </div>
 
-        <Percentage>
+        <div className="percentage">
           {`${book.progress}%`}
           <span
             style={{
@@ -130,26 +57,18 @@ const Book = (props) => {
           >
             Completed
           </span>
-        </Percentage>
+        </div>
       </MiddleWrapper>
 
       <RightWrapper>
-        <div style={{ color: '#777', paddingBottom: '0.4rem' }}>CURRENT CHAPTER</div>
-        <Chapter>Chapter 17</Chapter>
+        <div style={{ color: '#777', paddingBottom: '0.4rem' }}>
+          CURRENT CHAPTER
+        </div>
+        <p>Chapter 17</p>
         <ButtonFill>UPDATE PROGRESS</ButtonFill>
       </RightWrapper>
     </Wrapper>
   );
-};
-
-Book.defaultProps = {
-  book: {
-    category: 'Action',
-    title: 'The Hunger Games',
-    author: 'Suzanne Collins',
-    id: Date.now(),
-    progress: 66,
-  },
 };
 
 Book.propTypes = {
@@ -157,9 +76,9 @@ Book.propTypes = {
     category: PropTypes.string.isRequired,
     title: PropTypes.string.isRequired,
     author: PropTypes.string.isRequired,
-    id: PropTypes.number.isRequired,
+    id: PropTypes.string.isRequired,
     progress: PropTypes.number.isRequired,
-  }),
+  }).isRequired,
 };
 
 export default Book;
