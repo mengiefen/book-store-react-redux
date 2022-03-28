8094d751f6b1f67d533662e8323d16477b5cea42
mengiefen <mengiefen@gmail.com>
2022-03-28 19:02:42 +0300

    update index.js for redux state management

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import rootReducer from './redux/configureStore';
 import './index.css';
-import App from './App';
+import App from './components/App';
+
+const store = createStore(rootReducer);
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </Router>
   </React.StrictMode>,
   document.getElementById('root'),
