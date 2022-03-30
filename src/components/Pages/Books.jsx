a030fec9af93c2d194570ed3eff5e1c4649bed21
mengiefen <mengiefen@gmail.com>
2022-03-31 00:21:43 +0300

    update Books.js file

--- src/components/Pages/Books.jsx
+++ src/components/Pages/Books.jsx
@@ -1,22 +1,16 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
 import Wrapper from './Books.styled';
 import Book from '../Book/Book';
 import Form from '../Form/Form';
 
-const Books = () => {
-  const books = useSelector((state) => state.books);
+const Books = () => (
+  <Wrapper>
+    <div className="book-item">
+      <Book />
+    </div>
 
-  return (
-    <Wrapper>
-      {books.map((book) => (
-        <div key={book.id} className="book-item">
-          <Book book={book} />
-        </div>
-      ))}
-      <Form />
-    </Wrapper>
-  );
-};
+    <Form />
+  </Wrapper>
+);
 
 export default Books;
