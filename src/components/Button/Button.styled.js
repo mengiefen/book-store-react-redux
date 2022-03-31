669ea8f887eb55a0d280748470d4e9d28df1520d
mengiefen <mengiefen@gmail.com>
2022-03-31 20:00:40 +0300

    update color proprty and font-weight

--- src/components/Button/Button.styled.js
+++ src/components/Button/Button.styled.js
@@ -1,14 +1,14 @@
 import styled from 'styled-components';
 
 const ButtonFill = styled.button`
-  background-color: ${({ theme }) => theme.colors.primary};
+  background-color: ${({ theme }) => theme.colors.blue};
   border: none;
   outline: none;
   border-radius: 4px;
-  color: ${({ theme }) => theme.colors.backgroundColor};
+  color: ${({ theme }) => theme.colors.light};
   padding: 0.801rem 1.188rem 0.886rem 1.375rem;
   box-shadow: 0 0 60px 0 rgba(0, 0, 0, 0.05);
-  font-weight: 600;
+  font-weight: 300;
   letter-spacing: 1.5px;
   cursor: pointer;
   transition: all 0.5s;
@@ -20,14 +20,16 @@ const ButtonFill = styled.button`
 `;
 
 const ButtonOutline = styled.button`
-  color: ${({ theme }) => theme.colors.lightPrimary};
+  color: ${({ theme }) => theme.colors.coolBlue};
   outline: none;
+  font-size: 0.813rem;
   border: none;
   background-color: transparent;
   cursor: pointer;
+  font-weight: 300;
 
   &:hover {
-    color: ${({ theme }) => theme.colors.primary};
+    color: ${({ theme }) => theme.colors.blue};
   }
 `;
 
